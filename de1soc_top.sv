c665c734d869ba4ac6dafbb0cf29f1529c445c39
edwardyliu <edwardyliu.interest@gmail.com>
2018-02-14 14:04:02 -0500

    Added files and updated files via upload

--- de1soc_top.sv
+++ de1soc_top.sv
@@ -47,5 +47,49 @@ end
 // INSTANTIATE QSYS SYSTEM HERE
 //
 
+/* PART 1:
+ *
+ *	nios_system u0 (
+ *		.clk_clk         (<connected-to-clk_clk>),         //      clk.clk
+ *		.leds_export     (<connected-to-leds_export>),     //     leds.export
+ *		.reset_reset_n   (<connected-to-reset_reset_n>),   //    reset.reset_n
+ *		.switches_export (<connected-to-switches_export>)  // switches.export
+ *	);
+ *
+ *
+ *
+ * PART 3:
+ *
+ * nios_system u0 (
+ *    .clk_clk                                 (<connected-to-clk_clk>),                                 //                              clk.clk
+ *    .leds_export                             (<connected-to-leds_export>),                             //                             leds.export
+ *    .reset_reset_n                           (<connected-to-reset_reset_n>),                           //                            reset.reset_n
+ *    .switches_export                         (<connected-to-switches_export>),                         //                         switches.export
+ *    .line_drawer_module_0_vga_b_export       (<connected-to-line_drawer_module_0_vga_b_export>),       //       line_drawer_module_0_vga_b.export
+ *    .line_drawer_module_0_vga_blank_n_export (<connected-to-line_drawer_module_0_vga_blank_n_export>), // line_drawer_module_0_vga_blank_n.export
+ *    .line_drawer_module_0_vga_clk_export     (<connected-to-line_drawer_module_0_vga_clk_export>),     //     line_drawer_module_0_vga_clk.export
+ *    .line_drawer_module_0_vga_g_export       (<connected-to-line_drawer_module_0_vga_g_export>),       //       line_drawer_module_0_vga_g.export
+ *    .line_drawer_module_0_vga_hs_export      (<connected-to-line_drawer_module_0_vga_hs_export>),      //      line_drawer_module_0_vga_hs.export
+ *    .line_drawer_module_0_vga_r_export       (<connected-to-line_drawer_module_0_vga_r_export>),       //       line_drawer_module_0_vga_r.export
+ *    .line_drawer_module_0_vga_sync_n_export  (<connected-to-line_drawer_module_0_vga_sync_n_export>),  //  line_drawer_module_0_vga_sync_n.export
+ *    .line_drawer_module_0_vga_vs_export      (<connected-to-line_drawer_module_0_vga_vs_export>)       //      line_drawer_module_0_vga_vs.export
+ *	);
+*/
+
+nios_system NiosII
+(
+	.clk_clk(CLOCK_50),
+	.leds_export(LEDR[7:0]),
+	.reset_reset_n(KEY[0]),
+	.switches_export(SW[7:0]),
+	.line_drawer_module_0_vga_b_export(VGA_B),
+	.line_drawer_module_0_vga_blank_n_export(VGA_BLANK_N),
+	.line_drawer_module_0_vga_clk_export(VGA_CLK),
+	.line_drawer_module_0_vga_g_export(VGA_G),
+	.line_drawer_module_0_vga_hs_export(VGA_HS),
+	.line_drawer_module_0_vga_r_export(VGA_R),
+	.line_drawer_module_0_vga_sync_n_export(VGA_SYNC_N),
+	.line_drawer_module_0_vga_vs_export(VGA_VS)
+);
 
 endmodule
